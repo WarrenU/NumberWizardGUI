862f15c317789b81704c94c0961d8f9625de2e86
Unknown <warren.urbina@gmail.com>
2017-04-24 21:31:14 -0700

    Simple scene + Text Button and Level Manager obj + script setup.

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	public void load_level(string level_name){
+		Application.LoadLevel(level_name);
+	}
+	
+	public void quit(){
+		Application.Quit();
+	}
+}
